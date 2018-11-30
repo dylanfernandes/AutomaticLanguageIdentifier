45dd4244167b3d8f6575879991b1c79c6eca1c81
dylanfernandes <dylangfernandes@gmail.com>
2018-11-30 15:38:01 -0500

    Completed implementation of bigram data collection

--- Code/bigram.py
+++ Code/bigram.py
@@ -12,26 +12,45 @@ class BigramModel:
             self.train(input_str)
 
     def train(self, input_str):
-        prev = None
-        prevDict = {}
+        current = None
+        occDict = {}
         if input_str:
             self.training_size = len(input_str)
-            for char in input_str:
-                if prev == None:
-                    prev = char
-                else:
-                    if char in self.char_dict:
-                        prevDict = self.char_dict[char]
-                        if prev in prevDict:
-                            prevDict[prev] += 1
+            for nextChar in input_str:
+                #Skip first char
+                if current != None:
+                    if current in self.char_dict:
+                        occDict = self.char_dict[current]
+                        #print(str(prev) + "||" + str(occDict))
+                        if nextChar in occDict:
+                            #print("Seen char")
+                            occDict[nextChar] += 1
                         else:
-                            prevDict[prev] = 1
-                        self.char_dict[char] = prevDict
+                            occDict[nextChar] = 1
+                        occDict["total"] += 1
+                        self.char_dict[current] = occDict
                     else:
-                        self.char_dict[char] = {prev:1}
+                        #create dictionary for new char
+                        occDict = {}
+                        occDict[nextChar] = 1
+                        occDict["total"] = 1
+                        self.char_dict[current] = occDict
+                        #print(str(current) + "||" + str(self.char_dict[current]))
+                #set current char to previous
+                current = nextChar
+            
+            if nextChar in self.char_dict:
+                self.char_dict[nextChar]["total"] += 1
+            else:
+                self.char_dict[nextChar] = {"total":1}
 					
-            #for char in self.char_dict:
-				#self.probs[char] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
+            for char in self.char_dict:
+                print(str(char) + ": " + str(self.char_dict[char]))
+                #for char in self.prev_dict[prev]:
+                    #print(char)
+				
+				#self.probs[prev] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
 	
     #def calc_prob(self, char_count=0, smoothing=BigramModel.SMOOTHING, total_count=0, vocab_size=BigramModel.VOCAB_SIZE):
 		#return (char_count + smoothing) / (total_count + smoothing * vocab_size)
+			
