190e13fba1cc4561f40a07220873eeb627a365dd
dylanfernandes <dylangfernandes@gmail.com>
2018-11-30 15:50:45 -0500

    Modularize implementation

--- Code/bigram.py
+++ Code/bigram.py
@@ -12,10 +12,22 @@ class BigramModel:
             self.train(input_str)
 
     def train(self, input_str):
+        self.gatherData(input_str)
+        for char in self.char_dict:
+            print(str(char) + ": " + str(self.char_dict[char]))
+            #for char in self.prev_dict[prev]:
+                #print(char)
+                #self.probs[prev] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
+
+
+    def gatherData(self, input_str):
         current = None
+        #Stores dictionary of characters following current category
         occDict = {}
+        testString = "aaaabbcdef"
         if input_str:
             self.training_size = len(input_str)
+            #dictionary built from perspective of last character
             for nextChar in input_str:
                 #Skip first char
                 if current != None:
@@ -38,18 +50,13 @@ class BigramModel:
                         #print(str(current) + "||" + str(self.char_dict[current]))
                 #set current char to previous
                 current = nextChar
-            
+
+            #no next character, just increment total
             if nextChar in self.char_dict:
                 self.char_dict[nextChar]["total"] += 1
             else:
                 self.char_dict[nextChar] = {"total":1}
 					
-            for char in self.char_dict:
-                print(str(char) + ": " + str(self.char_dict[char]))
-                #for char in self.prev_dict[prev]:
-                    #print(char)
-				
-				#self.probs[prev] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
 	
     #def calc_prob(self, char_count=0, smoothing=BigramModel.SMOOTHING, total_count=0, vocab_size=BigramModel.VOCAB_SIZE):
 		#return (char_count + smoothing) / (total_count + smoothing * vocab_size)
