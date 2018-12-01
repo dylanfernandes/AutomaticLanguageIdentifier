917c5ebd685f546b3fdb9a9bf01007fd28a3c729
dylanfernandes <dylangfernandes@gmail.com>
2018-12-01 01:50:37 -0500

    Finished Train method tests

--- Tests/bigram_tests.py
+++ Tests/bigram_tests.py
@@ -7,6 +7,24 @@ class TestBigram(unittest.TestCase):
     def testTrain(self):
         testData = BigramModel("aaaabbcdef")
         self.assertEqual(testData.trained, True)
+        self.assertEqual(len(testData.char_dict.keys()), 6)
+        self.assertEqual(testData.char_dict["a"]["total"], 4)
+        self.assertEqual(testData.char_dict["a"]["a"], 3)
+        self.assertEqual(testData.char_dict["a"]["b"], 1)
+        self.assertEqual(testData.char_dict["b"]["total"], 2)
+        self.assertEqual(testData.char_dict["b"]["b"], 1)
+        self.assertEqual(testData.char_dict["b"]["c"], 1)
+        self.assertEqual(testData.char_dict["c"]["total"], 1)
+        self.assertEqual(testData.char_dict["c"]["d"], 1)
+        self.assertEqual(testData.char_dict["d"]["total"], 1)
+        self.assertEqual(testData.char_dict["d"]["e"], 1)
+        self.assertEqual(testData.char_dict["e"]["total"], 1)
+        self.assertEqual(testData.char_dict["e"]["f"], 1)
+        self.assertEqual(testData.char_dict["f"]["total"], 1)
+
+        self.assertEqual(len(testData.char_dict["f"].keys()), 1)
+        self.assertEqual("e" in testData.char_dict["f"], False)
+        self.assertEqual("c" in testData.char_dict["a"], False)
 
 if __name__ == '__main__':
         unittest.main()
