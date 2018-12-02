6a1db31c8a1b7e56505d9997c112e98a01aeeaa5
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-12-01 19:51:06 -0800

    Completed Unigram class and tests

--- Code/unigram.py
+++ Code/unigram.py
@@ -1,32 +1,65 @@
 import pickle
+from math import log10
+
 
 # TODO: when testing, try to load file from pickle, train if file does not exist.
 class UnigramModel:
-	
-	SMOOTHING = 0.5
-	VOCAB_SIZE = 26
-	
-	def __init__(self, input_str=None):
-		self.char_dict = {}
-		self.probs = {}
-		self.training_size = 0
-		if input_str:
-			self.train(input)
-	
-	def train(self, input_str):
-		# TODO: save to pickle
-		if input_str:
-			self.training_size = len(input_str)
-			for char in input:
-				if char in self.char_dict:
-					self.char_dict[char] += 1
-				else:
-					self.char_dict[char] = 1
-					
-			for char in self.char_dict:
-				self.probs[char] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
-	
-	def calc_prob(self, char_count=0, smoothing=UnigramModel.SMOOTHING, total_count=0, vocab_size=UnigramModel.VOCAB_SIZE):
-		return (char_count + smoothing) / (total_count + smoothing * vocab_size)
-	
-				
+    SMOOTHING = 0.5
+    VOCAB_SIZE = 26
+
+    def __init__(self, input_str=None, smooth=False):
+        self.char_dict = {}
+        self.probs = {}
+        self.training_size = 0
+        self.trained = False
+        self.smooth = smooth
+        if input_str:
+            self.train(input_str)
+
+    def train(self, input_str):
+        # TODO: save to pickle
+
+        if input_str:
+            self.training_size = len(input_str)
+            for char in input_str:
+                if char in self.char_dict:
+                    self.char_dict[char] += 1
+                else:
+                    self.char_dict[char] = 1
+
+            for char in self.char_dict:
+                if self.smooth:
+                    self.probs[char] = self.calc_prob(char_count=self.char_dict[char],
+                                                      total_count=self.training_size,
+                                                      smoothing=UnigramModel.SMOOTHING,
+                                                      vocab_size=UnigramModel.VOCAB_SIZE)
+                else:
+                    self.probs[char] = self.calc_prob(char_count=self.char_dict[char], total_count=self.training_size)
+
+            self.trained = True
+
+    def prob_sentence(self, input_str):
+
+        total_prob = 0
+        if self.trained:
+            for char in input_str:
+                if char in self.char_dict:
+                    total_prob += self.probs[char]
+                elif self.smooth:
+                    total_prob += self.calc_prob(0, total_count=self.training_size, smoothing=UnigramModel.SMOOTHING,
+                                                 vocab_size=UnigramModel.VOCAB_SIZE)
+        else:
+            print('Model has not been trained!')
+
+        return total_prob
+
+    @staticmethod
+    def calc_prob(char_count, total_count, smoothing=0.0, vocab_size=0.0):
+
+        if smoothing < 0.0:
+            smoothing = UnigramModel.SMOOTHING
+
+        if vocab_size < 0.0:
+            vocab_size = UnigramModel.VOCAB_SIZE
+
+        return log10((char_count + smoothing) / (total_count + smoothing * vocab_size))
---
+++ Tests/unigram_test.py
@@ -0,0 +1,68 @@
+import unittest
+from Code.unigram import UnigramModel
+from math import log10
+
+
+class UnigramTests(unittest.TestCase):
+
+    train_str = "zahhjewjkojfcfdssd"
+    input_str = "aaabbcdddd"
+
+    def test_train(self):
+
+        train_str_unique = UnigramTests.str_unique_chars(UnigramTests.train_str)
+        unigram_model = UnigramModel(UnigramTests.train_str)
+        self.assertEqual(len(unigram_model.char_dict), len(train_str_unique))
+        self.assertEqual(len(unigram_model.probs), len(unigram_model.char_dict))
+
+        total_count = 0
+        for char_count in unigram_model.char_dict.values():
+            total_count += char_count
+
+        self.assertEqual(total_count, len(UnigramTests.train_str))
+
+        # test one of the probability calculations
+        # For character 'z'
+        prob_test = log10(1.0 / len(UnigramTests.train_str))
+        self.assertEqual(unigram_model.probs['z'], prob_test)
+
+        self.assertTrue(unigram_model.trained)
+
+    def test_unigram_train_smoothed(self):
+        unigram_model = UnigramModel(UnigramTests.train_str, True)
+
+        # test one of the probability calculations
+        # For character 'z'
+        prob_test = log10((1.0 + UnigramModel.SMOOTHING) /
+                          (len(UnigramTests.train_str) + UnigramModel.SMOOTHING * UnigramModel.VOCAB_SIZE))
+        self.assertEqual(unigram_model.probs['z'], prob_test)
+
+    def test_unigram_test_str(self):
+        unigram_model = UnigramModel(UnigramTests.train_str)
+
+        # Manually calculated
+        expected_val = -8.838
+        self.assertAlmostEqual(unigram_model.prob_sentence(UnigramTests.input_str), expected_val, places=2)
+
+    def test_unigram_test_str_smooth(self):
+        unigram_model = UnigramModel(UnigramTests.train_str, True)
+
+        # Manually calculated
+        expected_val = -13.22
+        self.assertAlmostEqual(unigram_model.prob_sentence(UnigramTests.input_str), expected_val, places=2)
+
+    def test_unigram_test_str_untrained(self):
+        unigram_model = UnigramModel()
+        unigram_model_smooth = UnigramModel(smooth=True)
+
+        self.assertAlmostEqual(unigram_model.prob_sentence(UnigramTests.input_str), 0.0, places=2)
+        self.assertAlmostEqual(unigram_model_smooth.prob_sentence(UnigramTests.input_str), 0.0, places=2)
+
+    # Useful functions
+    @staticmethod
+    def str_unique_chars(input_str):
+        return ''.join(set(input_str))
+
+
+if __name__ == '__main__':
+    unittest.main()
