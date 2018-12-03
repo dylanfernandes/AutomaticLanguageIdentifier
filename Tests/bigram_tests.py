da3ca97b3477b989ba6d98e0556426c93ffb1456
dylanfernandes <dylangfernandes@gmail.com>
2018-12-02 22:14:50 -0500

    Completed smoothing test

--- Tests/bigram_tests.py
+++ Tests/bigram_tests.py
@@ -28,45 +28,44 @@ class TestBigram(unittest.TestCase):
         self.assertEqual("e" in testData.char_dict["f"], True)
         self.assertEqual("c" in testData.char_dict["a"], False)
     
-    # def testSmoothing(self):
-    #     testData = BigramModel("aaaabbcdef")
-    #     smooth = testData.SMOOTHING
-    #     testData.smooth_char_dict("aagbef")
-    #     self.assertEqual(len(testData.char_dict.keys()), 7)
-    #     self.assertEqual(testData.char_dict["a"]["g"], smooth)
-    #     self.assertEqual(testData.char_dict["a"]["total"], 4 + smooth)
-    #     self.assertEqual(testData.char_dict["g"]["total"], smooth)
-    #     self.assertEqual(testData.char_dict["g"]["b"], smooth)
-    #     self.assertEqual(len(testData.char_dict["g"].keys()), 2)
-    #     self.assertEqual(testData.char_dict["b"]["e"], smooth)
-    #     self.assertEqual(testData.char_dict["b"]["total"], 2 + smooth)
+    def test_smoothing(self):
+        testData = BigramModel("aaaabbcdef")
+        testData.smooth_char_dict("aagbef")
+        self.assertEqual(len(testData.char_dict.keys()), 7)
+        self.assertEqual(testData.char_dict["g"]["a"], 0)
+        self.assertEqual(testData.char_dict["a"]["total"], 4)
+        self.assertEqual(testData.char_dict["g"]["total"], 0)
+        self.assertEqual(testData.char_dict["b"]["g"], 0)
+        self.assertEqual(len(testData.char_dict["g"].keys()), 2)
+        self.assertEqual(testData.char_dict["e"]["b"], 0)
+        self.assertEqual(testData.char_dict["b"]["total"], 2)
+        self.assertEqual(testData.char_dict["e"]["d"], 1)
+        self.assertEqual(testData.char_dict["e"]["total"], 1)
+        self.assertEqual(testData.char_dict["f"]["e"], 1)
+        self.assertEqual(testData.char_dict["f"]["total"], 1)
 
-    #     self.assertEqual(testData.char_dict["e"]["total"], 1)
-    #     self.assertEqual(testData.char_dict["e"]["f"], 1)
-    #     self.assertEqual(testData.char_dict["f"]["total"], 1)
-
-    # def testCalculateProbabilities(self):
+    # def test_calculate_probabilities(self):
     #     testData = BigramModel("aaaabbcdef")
-    #     smooth = testData.SMOOTHING
+    #     smooth = testData.smoothing
     #     testData.smooth_char_dict("aagbef")
     #     testData.calculateProbablities()
     #     self.assertEqual(len(testData.probs.keys()), 6)
     #     #last char preceding end of string neglected
     #     self.assertEqual("f" in testData.probs, False)
-    #     self.assertEqual(testData.probs["a"]["a"], 3/(4+smooth))
-    #     self.assertEqual(testData.probs["a"]["g"], smooth/(4+smooth))
-    #     self.assertEqual(testData.probs["g"]["b"], 1)
-    #     self.assertEqual(testData.probs["b"]["e"], smooth/(2+smooth))
-    #     self.assertEqual(testData.probs["e"]["f"], 1)
+    #     self.assertEqual(testData.probs["a"]["a"], (3+smooth)/(4+(smooth*7)))
+    #     self.assertEqual(testData.probs["a"]["g"], smooth/(4+smooth*7))
+    #     self.assertEqual(testData.probs["g"]["b"], smooth/(smooth*7))
+    #     self.assertEqual(testData.probs["b"]["e"], smooth/(2+smooth*7))
+    #     self.assertEqual(testData.probs["e"]["f"], (1+smooth)/(1+(smooth*7)))
     
-    # def testGetStringProb(self):
+    # def test_get_string_prob(self):
     #     testData = BigramModel("aaaabbcdef")
-    #     smooth = testData.SMOOTHING
+    #     smooth = testData.smoothing
     #     testData.smooth_char_dict("aagbef")
     #     testData.calculateProbablities()
-    #     val = math.log10(3/(4+smooth)) + math.log10(smooth/(4+smooth)) + math.log10(1) + math.log10(smooth/(2+smooth)) + math.log10(1)
+    #     val = math.log10((3+smooth)/(4+(smooth*7))) + math.log10(smooth/(4+smooth*7)) + math.log10(smooth/(smooth*7)) + math.log10(smooth/(2+smooth*7)) + math.log10((1+smooth)/(1+(smooth*7)))
     #     val = -1 * val
-    #     self.assertEqual(testData.get_string_prob("aagbef"), val)
+    #     self.assertAlmostEqual(testData.get_string_prob("aagbef"), val)
 
 
 if __name__ == '__main__':
