6e39a06c2b86576fb90b833a6a519bb648b32261
dylanfernandes <dylangfernandes@gmail.com>
2018-12-02 22:08:37 -0500

    Implementation of bigram_prev with training test

---
+++ Tests/bigram_tests.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.insert(0, '../Code')
+import unittest
+import math
+
+from bigram import BigramModel
+
+class TestBigram(unittest.TestCase):
+    def testTrain(self):
+        testData = BigramModel("aaaabbcdef")
+        self.assertEqual(testData.trained, True)
+        self.assertEqual(len(testData.char_dict.keys()), 6)
+        self.assertEqual(testData.char_dict["a"]["total"], 4)
+        self.assertEqual(testData.char_dict["a"]["a"], 3)
+        self.assertEqual(testData.char_dict["b"]["total"], 2)
+        self.assertEqual(testData.char_dict["b"]["a"], 1)
+        self.assertEqual(testData.char_dict["b"]["b"], 1)
+        self.assertEqual(testData.char_dict["c"]["total"], 1)
+        self.assertEqual(testData.char_dict["c"]["b"], 1)
+        self.assertEqual(testData.char_dict["d"]["total"], 1)
+        self.assertEqual(testData.char_dict["d"]["c"], 1)
+        self.assertEqual(testData.char_dict["e"]["total"], 1)
+        self.assertEqual(testData.char_dict["e"]["d"], 1)
+        self.assertEqual(testData.char_dict["f"]["total"], 1)
+        self.assertEqual(testData.char_dict["f"]["e"], 1)
+
+        self.assertEqual(len(testData.char_dict["f"].keys()), 2)
+        self.assertEqual("e" in testData.char_dict["f"], True)
+        self.assertEqual("c" in testData.char_dict["a"], False)
+    
+    # def testSmoothing(self):
+    #     testData = BigramModel("aaaabbcdef")
+    #     smooth = testData.SMOOTHING
+    #     testData.smooth_char_dict("aagbef")
+    #     self.assertEqual(len(testData.char_dict.keys()), 7)
+    #     self.assertEqual(testData.char_dict["a"]["g"], smooth)
+    #     self.assertEqual(testData.char_dict["a"]["total"], 4 + smooth)
+    #     self.assertEqual(testData.char_dict["g"]["total"], smooth)
+    #     self.assertEqual(testData.char_dict["g"]["b"], smooth)
+    #     self.assertEqual(len(testData.char_dict["g"].keys()), 2)
+    #     self.assertEqual(testData.char_dict["b"]["e"], smooth)
+    #     self.assertEqual(testData.char_dict["b"]["total"], 2 + smooth)
+
+    #     self.assertEqual(testData.char_dict["e"]["total"], 1)
+    #     self.assertEqual(testData.char_dict["e"]["f"], 1)
+    #     self.assertEqual(testData.char_dict["f"]["total"], 1)
+
+    # def testCalculateProbabilities(self):
+    #     testData = BigramModel("aaaabbcdef")
+    #     smooth = testData.SMOOTHING
+    #     testData.smooth_char_dict("aagbef")
+    #     testData.calculateProbablities()
+    #     self.assertEqual(len(testData.probs.keys()), 6)
+    #     #last char preceding end of string neglected
+    #     self.assertEqual("f" in testData.probs, False)
+    #     self.assertEqual(testData.probs["a"]["a"], 3/(4+smooth))
+    #     self.assertEqual(testData.probs["a"]["g"], smooth/(4+smooth))
+    #     self.assertEqual(testData.probs["g"]["b"], 1)
+    #     self.assertEqual(testData.probs["b"]["e"], smooth/(2+smooth))
+    #     self.assertEqual(testData.probs["e"]["f"], 1)
+    
+    # def testGetStringProb(self):
+    #     testData = BigramModel("aaaabbcdef")
+    #     smooth = testData.SMOOTHING
+    #     testData.smooth_char_dict("aagbef")
+    #     testData.calculateProbablities()
+    #     val = math.log10(3/(4+smooth)) + math.log10(smooth/(4+smooth)) + math.log10(1) + math.log10(smooth/(2+smooth)) + math.log10(1)
+    #     val = -1 * val
+    #     self.assertEqual(testData.get_string_prob("aagbef"), val)
+
+
+if __name__ == '__main__':
+        unittest.main()
