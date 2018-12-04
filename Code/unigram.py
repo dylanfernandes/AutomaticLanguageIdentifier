d30c831572fbde09bf4e1c920507c5a10ac0b846
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-12-03 23:51:50 -0800

    Fixed training to increment training size
    Increments training size for each input string trained.

--- Code/unigram.py
+++ Code/unigram.py
@@ -15,14 +15,12 @@ class UnigramModel:
         if input_str:
             self.train(input_str)
 
-    def train(self, input_str, append=False):
+    def train(self, input_str):
         # TODO: save to pickle
 
         if input_str:
-            if append:
-                self.training_size += len(input_str)
-            else:
-                self.training_size = len(input_str)
+
+            self.training_size += len(input_str)
             for char in input_str:
                 if char in self.char_dict:
                     self.char_dict[char] += 1
