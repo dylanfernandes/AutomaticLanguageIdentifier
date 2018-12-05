7065fd841caca980a40e2035ccae23b1c169d7fc
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-12-04 19:37:34 -0800

    Created the latin experiment file

---
+++ Experiments/latin_exp.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(0, '../')
+from Code.main_script import *
+
+LATIN_SENTENCES = [
+    "Carpe diem",
+    "Veni, vidi, vici",
+    "Felix culpa",
+    "Vox nihili",
+    "Sic semper tyrranis",
+    "E pluribus unum",
+    "Semper fidelis",
+    "Ad nauseam",
+    "Lorem ipsum dolor sit amet, sit quis ipsum sed excepteur volutpat",
+    "Barba non facit philosophum"
+]
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
